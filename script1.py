fc3363fa7d76e6caee40b376cb2768d060e4792e
nicholaskier <nicholaskier@gmail.com>
2021-04-05 10:43:11 -0500

    first commit

---
+++ script1.py
@@ -0,0 +1,23 @@
+
+from tkinter import * 
+# automatically comes installed with python
+# window and widgets
+# import sys
+# import os
+
+# if os.environ.get('DISPLAY','') == '':
+#     print('no display found')
+#     os.environ.__setitem__('DISPLAY', ':0.0')
+
+window=Tk()
+# create an empty window
+b1=Button(window,text="Execute")
+b1.grid(row=0,column=0)
+
+e1=Entry(window)
+e1.grid(row=0,column=1)
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=0,column=2)
+
+window.mainloop()
