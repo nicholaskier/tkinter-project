27bdc936653bc1310ab46c6366707ea846bec92e
nicholaskier <nicholaskier@gmail.com>
2021-04-05 12:25:50 -0500

    second practice

--- script1.py
+++ script1.py
@@ -1,4 +1,3 @@
-
 from tkinter import * 
 # automatically comes installed with python
 # window and widgets
@@ -11,10 +10,18 @@ from tkinter import *
 
 window=Tk()
 # create an empty window
-b1=Button(window,text="Execute")
+
+def km_to_mi():
+    print(e1_value.get())
+    miles=float(e1_value.get())*1.6
+    t1.insert(END,miles)
+
+b1=Button(window,text="Execute",command=km_to_mi)
+# referencing the function, but not calling it
 b1.grid(row=0,column=0)
 
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window,textvariable=e1_value)
 e1.grid(row=0,column=1)
 
 t1=Text(window,height=1,width=20)
